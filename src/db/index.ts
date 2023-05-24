da81c011464c991a5351375ebbc4183217f60f4c
Tareez Ghandour <tareezghandour@hotmail.com>
2023-05-24 20:52:32 +0300

    change link

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", true);
 
 const connectDb = async () => {
-  await mongoose.connect("mongodb://localhost/stocks-app");
-  // await mongoose.connect(
-  //   "mongodb+srv://Tareez:Ghandour@stocks.memk9pu.mongodb.net/?retryWrites=true&w=majority"
-  // );
+  // await mongoose.connect("mongodb://localhost/stocks-app");
+  await mongoose.connect(
+    "mongodb+srv://Tareez:Ghandour@stocks.memk9pu.mongodb.net/?retryWrites=true&w=majority"
+  );
 };
 export { connectDb };
